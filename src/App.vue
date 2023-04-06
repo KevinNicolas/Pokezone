3074c205954018a9bfd78a6ad013e51182aadc0c
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2023-04-06 12:28:49 +0000

    chore: :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Loader from "~/components/Loader.component.vue";
+</script>
+
+<template>
+  <div>
+    <span>App</span>
+  </div>
+</template>
+
+<style scoped></style>
